28c043ab9f5e72cbf4b421de12d73b6d77917a31
vaishnaviagarwal27 <61281973+vaishnaviagarwal27@users.noreply.github.com>
2020-03-28 11:59:17 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,18 @@
+class Ground{
+    constructor(x,y,width,height){
+        var options={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.height=height;
+        this.width=width;
+        World.add(world,this.body);
+     }
+    display(){
+        fill (180);
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+    
+    }
+
+}
